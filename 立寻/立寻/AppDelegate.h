b97f044f2f10c00c46350207fec99a26cf6c0954
mac_hao <mac_hao@mac-haodeMac-mini.local>
2017-05-23 00:12:13 +0800

    新建项目

---
+++ 立寻/立寻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  立寻
+//
+//  Created by mac_hao on 2017/5/23.
+//  Copyright © 2017年 杜浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 立寻/立寻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  立寻
+//
+//  Created by mac_hao on 2017/5/23.
+//  Copyright © 2017年 杜浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
